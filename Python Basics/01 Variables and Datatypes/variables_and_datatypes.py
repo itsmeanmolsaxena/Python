73adf439e14471261eb3cf57e1fb573f05fb1063
itsmeanmolsaxena <itsmeanmolsaxena@gmail.com>
2023-07-11 19:09:52 +0530

    variables and datatypes

---
+++ Python Basics/01 Variables and Datatypes/variables_and_datatypes.py
@@ -0,0 +1,37 @@
+# examples for variables and data types
+
+x = 100
+y = 100.10
+z = "String"
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print("data type of ", z, "is :", type(z))
+
+#example of list
+
+a = [19, 20, 21]
+
+print(a)
+print(type(a))
+
+# #example of tuple
+
+b = ("this", "is", "a", "tuple")
+
+print(b)
+print(type(b))
+
+#assign multiple value at once
+
+(j, k, l) = 1,1,1 
+
+#OR
+
+j = k = l = 1
+
+print(j,k,l)
