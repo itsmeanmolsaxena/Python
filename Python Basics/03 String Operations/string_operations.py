f1bb9429db9f36e86b0b6dc34d6546282cd5bd6f
itsmeanmolsaxena <itsmeanmolsaxena@gmail.com>
2023-07-11 19:12:14 +0530

    basic string operations

---
+++ Python Basics/03 String Operations/string_operations.py
@@ -0,0 +1,19 @@
+word = "Python"
+
+#extract the first letter
+
+print("First Letter in the",word , "is :-", word[0])
+
+#extract first 3 letters
+
+print("first 3 character are :-", word[0:3])
+
+#OR
+
+print(word[:3])
+
+#extract all letters after the first letter
+
+print("Letters after the first character are :-", word[1:])
+
+print("Length of the word", word, "is :-", len(word))
