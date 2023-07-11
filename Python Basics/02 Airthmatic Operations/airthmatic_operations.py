9388fe5e2548eeaa65fcbe4ca81826a32a6b65b0
itsmeanmolsaxena <itsmeanmolsaxena@gmail.com>
2023-07-11 19:11:28 +0530

    airthmatic operations

---
+++ Python Basics/02 Airthmatic Operations/airthmatic_operations.py
@@ -0,0 +1,8 @@
+a, b, c, d = 2, 4, 6, 8
+
+print("Sum =", a +b)
+print("Subtract = ", c -b)
+print("Multiply =", c * d)
+print("Divide = ", d / a)
+print("floor division = ", d // a)
+print("Modulus = ", d / a)
