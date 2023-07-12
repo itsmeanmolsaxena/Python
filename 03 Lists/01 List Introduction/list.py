fd1d77718d9ada7126c76156d7a2594b32308051
itsmeanmolsaxena <itsmeanmolsaxena@gmail.com>
2023-07-12 23:44:22 +0530

    basic list introduction

---
+++ 03 Lists/01 List Introduction/list.py
@@ -0,0 +1,22 @@
+# create a list
+# a list can hold any type of value and it is mutable means it's content can be changed
+
+#integer list
+int = [1, 2, 3, 4, 5]
+print(int)
+
+#character list
+char = ['a', 'e', 'i', 'o', 'u']
+print(char)
+
+#string list
+str = ["This", "is", "a", "string", "list"]
+print(str)
+
+#mixed list
+mix = [1, 'a', "List"]
+print(mix)
+
+#list in a form of matrix
+mat = [[1,2], ['x', 'y', 'z'], ["List"]]
+print(mat)
